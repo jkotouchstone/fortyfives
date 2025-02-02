1f4e038228929f4081881f104817926d2e02f7ad
jkotouchstone <jodonohue@touchstoneclosing.com>
2025-02-02 16:57:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from routes import fortyfives_bp  # Now this import will succeed.
+from routes import fortyfives_bp  # Import the blueprint
 
 app = Flask(__name__)
-app.register_blueprint(fortyfives_bp)  # Register the blueprint.
+app.register_blueprint(fortyfives_bp)  # Register the blueprint with the app
 
 if __name__ == '__main__':
     app.run(debug=True)
