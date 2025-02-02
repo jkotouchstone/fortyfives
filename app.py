a3159b3d269b54bfeef35446284fcc2dcf4d35fe
jkotouchstone <jodonohue@touchstoneclosing.com>
2025-02-02 10:39:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-# app.py
 from flask import Flask
 from routes import fortyfives_bp
 
 def create_app():
     app = Flask(__name__)
-    # Register the blueprint from routes.py
     app.register_blueprint(fortyfives_bp, url_prefix='/')
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    # Render typically runs python app.py by default. This is all you need.
     app.run(host="0.0.0.0", port=5000, debug=False)
