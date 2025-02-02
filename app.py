d72935b4f498366479092a24308531da311be3b3
jkotouchstone <jodonohue@touchstoneclosing.com>
2025-02-02 15:31:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from routes import fortyfives_bp
 
 app = Flask(__name__)
-
-# Register the blueprint
 app.register_blueprint(fortyfives_bp, url_prefix="/")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
