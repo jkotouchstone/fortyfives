faf040d907cca53e1436d8d85048581afaab2edf
jkotouchstone <jodonohue@touchstoneclosing.com>
2025-02-02 11:39:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes import fortyfives_bp
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(fortyfives_bp, url_prefix='/')
-    return app
+app = Flask(__name__)
+
+# Register the blueprint
+app.register_blueprint(fortyfives_bp, url_prefix="/")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000)
